13b011961816b427b69bd8e29a915cf33193aa12
Ayush Verma <ayush05verma@gmail.com>
2025-01-26 19:31:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function calculateMaturityAmount(){
+
+    
+
+    const principle = parseFloat(document.getElementById('principle').value);
+    const intersRate = parseFloat(document.getElementById('interestRate').value);
+    const tenure = parseFloat(document.getElementById('tenure').value);
+
+    
+
+    const maturityAmount = principle + (principle * intersRate * tenure)/100;
+
+    
+
+    document.getElementById('result').innerText = `Payout Value : ${maturityAmount.toFixed(2)}`;
+
+}
+
+
+document.getElementById('calculateBtn').addEventListener('click',calculateMaturityAmount);
